465cb439eec753c55607287f84f080d1712cab9a
Hosneara <127140568+hosneara-webdeveloper@users.noreply.github.com>
2023-08-23 12:00:16 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const products = require('./product.json')
+
+app.get('/', (req, res) => {
+    res.send('our backend server is ready for fullstack in web ')
+})
+
+app.get('/product', (req, res) => {
+    res.send(products)
+})
+
+app.get('/product/:id', (req, res) => {
+
+    const id = req.params.id;
+    console.log(id);
+    const product = products.find(product => product.id === id);
+    res.send(product)
+})
+
+app.listen(port, () => {
+    console.log(`Our server is on port: &{port}`);
+})
